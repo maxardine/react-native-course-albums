7b02e72aa8c2f50a0eaa43fcd0d525e9a0cf68d5
Davide Ardore <davide.ardore@gmail.com>
2017-09-03 20:27:21 +0100

    album list

---
+++ src/components/AlbumList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => {
+            this.setState({albums: response.data});
+        });
+    }
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <View>
+                <Text>hooray</Text>
+            </View>
+        );
+    }   
+}
+
+export default AlbumList;
