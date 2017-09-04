aab48a2ac770c81a480d10d61570b7f63e4ee1f4
Davide Ardore <davide.ardore@gmail.com>
2017-09-04 13:59:06 +0100

    cards

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -8,16 +9,21 @@ class AlbumList extends Component {
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
         .then(response => {
-            this.setState({albums: response.data});
+            this.setState({ albums: response.data });
         });
     }
 
+    renderAlbums() {
+        return this.state.albums.length === 0 ? <Text>Loading...</Text> : 
+        this.state.albums.map(
+            album => <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
-        console.log(this.state);
-        
         return (
             <View>
-                <Text>hooray</Text>
+                {this.renderAlbums()}
             </View>
         );
     }   
